fafd4018f0a795c7b1db9a0638609adadd37e4cb
LexHeadko <n-odes11rus@yandex.ru>
2023-03-02 17:59:02 +0200

    2 commit

--- src/main/java/com/skypro/calculator/controller/CalculatorController.java
+++ src/main/java/com/skypro/calculator/controller/CalculatorController.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/calculator")
 public class CalculatorController {
     private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
     @GetMapping
     public String hello() {
         return "Добро пожаловать в Калькулятор!";
@@ -17,18 +22,22 @@ public class CalculatorController {
 
     @GetMapping("/plus")
     public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+        int result = calculatorService.plus(num1, num2);
+        return num1 + "+" + num2 + "=" + result;
     }
     @GetMapping("/minus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    public String minus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int result = calculatorService.minus(num1, num2);
+        return num1 + "-" + num2 + "=" + result;
     }
     @GetMapping("/multiply")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int result = calculatorService.multiply(num1, num2);
+        return num1 + "*" + num2 + "=" + result;
     }
     @GetMapping("/divide")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        int result = calculatorService.divide(num1, num2);
+        return num1 + "/" + num2 + "=" + result;
     }
 }
--- src/main/java/com/skypro/calculator/service/CalculatorService.java
+++ src/main/java/com/skypro/calculator/service/CalculatorService.java
@@ -1,4 +1,7 @@
 package com.skypro.calculator.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
 }
