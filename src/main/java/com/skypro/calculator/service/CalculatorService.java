9766a1299f91411e11cddaf16d394201a04d085f
LexHeadko <n-odes11rus@yandex.ru>
2023-03-02 18:06:50 +0200

    3 commit

--- src/main/java/com/skypro/calculator/service/CalculatorService.java
+++ src/main/java/com/skypro/calculator/service/CalculatorService.java
@@ -4,4 +4,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorService {
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
 }
