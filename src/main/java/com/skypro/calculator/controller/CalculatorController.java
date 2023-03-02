dec5c22ffbc87ac1dc6762dc9ebcfc5851817494
LexHeadko <n-odes11rus@yandex.ru>
2023-03-02 18:59:05 +0200

    final commit

--- src/main/java/com/skypro/calculator/controller/CalculatorController.java
+++ src/main/java/com/skypro/calculator/controller/CalculatorController.java
@@ -1,36 +1,46 @@
 package com.skypro.calculator.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.skypro.calculator.exception.DivisionByZeroException;
+import com.skypro.calculator.service.CalculatorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @ExceptionHandler(DivisionByZeroException.class)
+    public ResponseEntity<?> nandleDivisionByZero(DivisionByZeroException e) {
+        return ResponseEntity.badRequest().body("Делить на ноль нельзя!!!");
+    }
     @GetMapping
     public String hello() {
         return "Добро пожаловать в Калькулятор!";
     }
 
     @GetMapping("/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    public String plus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        double result = calculatorService.plus(num1, num2);
+        return num1 + "+" + num2 + "=" + result;
     }
-    @GetMapping("/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    @GetMapping("/minus")
+    public String minus(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        double result = calculatorService.minus(num1, num2);
+        return num1 + "-" + num2 + "=" + result;
     }
-    @GetMapping("/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        double result = calculatorService.multiply(num1, num2);
+        return num1 + "*" + num2 + "=" + result;
     }
-    @GetMapping("/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
-    }
-    @GetMapping("/plus")
-    public String plus(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-
+    @GetMapping("/divide")
+    public String divide(@RequestParam("num1") double num1, @RequestParam("num2") double num2) {
+        double result = calculatorService.divide(num1, num2);
+        return num1 + "/" + num2 + "=" + result;
     }
 }
